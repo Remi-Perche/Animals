807ad35dc38c95e0d7496528c05a536c33b52a1a
Rémi Perche <rmi.percher@gmail.com>
2023-03-07 16:49:30 +0100

    Premières étapes animals

---
+++ Animal.cs
@@ -0,0 +1,22 @@
+﻿class Animal
+{
+    public string name;
+
+    public void Move()
+    {
+        Console.WriteLine("Move");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Sleep");
+    }
+    public Animal()
+    {
+
+    }
+    public Animal(string name)
+    {
+
+    }
+}
